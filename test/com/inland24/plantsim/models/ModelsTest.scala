2f1075cb62864a2beefe4fb8e6c8a25aad1638d0
joesan <codeintheopen@gmail.com>
2017-08-16 19:22:30 +0200

    added additional tests

--- test/com/inland24/plantsim/models/ModelsTest.scala
+++ test/com/inland24/plantsim/models/ModelsTest.scala
@@ -27,6 +27,8 @@ import scala.concurrent.duration._
 
 class ModelsTest extends FlatSpec {
 
+  behavior of "PowerPlantType"
+
   "PowerPlantType" should "fetch proper PowerPlantType instances" in {
     PowerPlantType.toString(PowerPlantType.OnOffType) === "OnOffType"
     PowerPlantType.toString(PowerPlantType.RampUpType) === "RampUpType"
@@ -142,5 +144,153 @@ class ModelsTest extends FlatSpec {
       Json.parse(expectedJsonUnknownType)
     ).get
     actualRampUpTypeCfg === unknownPlantCfg
+  }
+
+  behavior of "DispatchCommand"
+
+  "DispatchCommand#reads" should "fail parsing for a DispatchCommand invalid PowerPlantType" in {
+    val invalidPowerPlantType =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "turnOn",
+        |  "value" : true,
+        |  "powerPlantType" : "SomeShitPowerPlantType"
+        |}
+      """.stripMargin
+
+    Json.parse(invalidPowerPlantType).validate[DispatchCommand].fold(
+      _ => {
+        // Nothing to check
+      },
+      _ => {
+        fail(s"expected the parsing of an invalid DispatchCommand " +
+          s"$invalidPowerPlantType to fail, but it succeeded")
+      }
+    )
+  }
+
+  "DispatchCommand#reads" should "read the Json and parse a valid DispatchCommand for OnOffType" in {
+    val dispatchOnOffType =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "turnOn",
+        |  "value" : true,
+        |  "powerPlantType" : "OnOffType"
+        |}
+      """.stripMargin
+
+    Json.parse(dispatchOnOffType).validate[DispatchCommand].fold(
+      errors => {
+        fail(s"valid DispatchCommand should have successfully parsed, " +
+          s"but failed unexpectedly with errors $errors")
+      },
+      dispatchCommand => {
+        dispatchCommand.powerPlantId == 2
+      }
+    )
+  }
+
+  "DispatchCommand#reads" should "error when parsing an invalid DispatchCommand for OnOffType" in {
+    val invalidCommand =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "some shit",
+        |  "value" : true,
+        |  "powerPlantType" : "OnOffType"
+        |}
+      """.stripMargin
+
+    val invalidValue =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "turnOn",
+        |  "value" : "XXX",
+        |  "powerPlantType" : "OnOffType"
+        |}
+      """.stripMargin
+
+    Json.parse(invalidCommand).validate[DispatchCommand].fold(
+      _ => {
+        // Nothing to check!
+      },
+      _ => {
+        fail(s"expected the parsing of an invalid DispatchCommand $invalidCommand to fail, but it succeeded")
+      }
+    )
+
+    Json.parse(invalidValue).validate[DispatchCommand].fold(
+      _ => {
+        // Nothing to check!
+      },
+      _ => {
+        fail(s"expected the parsing of an invalid DispatchCommand $invalidValue to fail, but it succeeded")
+      }
+    )
+  }
+
+  "DispatchCommand#reads" should "read the Json and parse a valid DispatchCommand for RampUpType" in {
+    val dispatchRampUpType =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "dispatch",
+        |  "value" : 20.0,
+        |  "powerPlantType" : "RampUpType"
+        |}
+      """.stripMargin
+
+    Json.parse(dispatchRampUpType).validate[DispatchCommand].fold(
+      errors => {
+        fail(s"valid DispatchCommand should have successfully parsed, " +
+          s"but failed unexpectedly with errors $errors")
+      },
+      dispatchCommand => {
+        dispatchCommand.powerPlantId == 2
+      }
+    )
+  }
+
+  "DispatchCommand#reads" should "error when parsing an invalid DispatchCommand for RampUpType" in {
+    val invalidCommand =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "some shit",
+        |  "value" : 10,
+        |  "powerPlantType" : "RampUpType"
+        |}
+      """.stripMargin
+
+    val invalidValue =
+      """
+        |{
+        |  "powerPlantId" : 2,
+        |  "command" : "dispatch",
+        |  "value" : true,
+        |  "powerPlantType" : "RampUpType"
+        |}
+      """.stripMargin
+
+    Json.parse(invalidCommand).validate[DispatchCommand].fold(
+      _ => {
+        // Nothing to check!
+      },
+      _ => {
+        fail(s"expected the parsing of an invalid DispatchCommand $invalidCommand to fail, but it succeeded")
+      }
+    )
+
+    Json.parse(invalidValue).validate[DispatchCommand].fold(
+      _ => {
+        // Nothing to check!
+      },
+      _ => {
+        fail(s"expected the parsing of an invalid DispatchCommand $invalidValue to fail, but it succeeded")
+      }
+    )
   }
 }
