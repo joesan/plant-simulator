b744edac4dc973b4bf49dd2b44908c788a35d5d9
joesan <codeintheopen@gmail.com>
2020-02-27 18:52:00 +0100

    modified docker build

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ $TRAVIS_BRANCH == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin;
-  docker build -t $DOCKER_APP_NAME;
+  docker build -t $DOCKER_APP_NAME .;
   docker images;
   docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
   echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL";
