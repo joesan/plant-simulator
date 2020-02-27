52a4d60763fc011eb70933f88078668c22ac9469
joesan <codeintheopen@gmail.com>
2020-02-27 18:37:37 +0100

    modified docker build and push script

--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 if [ $TRAVIS_BRANCH == "master" ]; then
-  sbt docker:publishLocal;
-  docker login -u $DOCKER_REGISTRY_USERNAME -p $DOCKER_REGISTRY_PASSWORD $DOCKER_REGISTRY_URL;
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin;
+  docker build -t $DOCKER_APP_NAME;
   docker images;
+  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
   echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL";
   # We wait for 5 minutes until we opt out
   docker push $DOCKER_TAG_NAME;
+  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
 fi
