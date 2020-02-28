3fb80eb50c8308bf4b2191ff830342c540020a08
joesan <codeintheopen@gmail.com>
2020-02-28 11:08:48 +0100

    fix Docker related push errors

--- docker_push.sh
+++ docker_push.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 if [ $TRAVIS_BRANCH == "master" ]; then
-  echo "DOCKER_REGISTRY_USERNAME = "$DOCKER_REGISTRY_USERNAME
-  echo "DOCKER_APP_NAME          = "$DOCKER_APP_NAME
-  echo "DOCKER_REGISTRY_URL      = "$DOCKER_REGISTRY_URL
-  echo "TRAVIS_BUILD_NUMBER      = "$TRAVIS_BUILD_NUMBER
-
   docker build -t $DOCKER_APP_NAME .;
   docker images;
   docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
 
-  echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_BUILD_NUMBER";
-  echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin docker.io && docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
+  echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin docker.io
+  echo "Successfully logged into Docker hub <<hub.docker.com>>"
+
+  echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_BUILD_NUMBER"; 
+  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
 
   echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag latest";
   docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
