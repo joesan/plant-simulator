4785d40d25665e53e361f7a8fa5b5c6b2067e62d
joesan <codeintheopen@gmail.com>
2020-02-28 02:06:29 +0100

    fix docker error

--- docker_push.sh
+++ docker_push.sh
@@ -3,9 +3,9 @@ if [ $TRAVIS_BRANCH == "master" ]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin;
   docker build -t $DOCKER_APP_NAME .;
   docker images;
-  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:latest;
+  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
   echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL";
-  # We wait for 5 minutes until we opt out
-  docker push $DOCKER_TAG_NAME;
+  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
+  docker tag $DOCKER_APP_NAME $DOCKER_USER/$DOCKER_APP_NAME:latest;
   docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:latest;
 fi
