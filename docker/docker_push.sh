15e3507f1889b6405c6f6a57558f0b19d88ba036
joesan <codeintheopen@gmail.com>
2020-12-12 21:40:04 +0100

    addad a new tag

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-if [ $TRAVIS_BRANCH == "master" ]; then
+#if [ $TRAVIS_BRANCH == "master" ]; then
+if [ -n "$TRAVIS_TAG"  ]; then
   docker build -t $DOCKER_APP_NAME .;
   docker images;
-  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
+  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_TAG;
 
   echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin docker.io
   echo "Successfully logged into Docker hub <<hub.docker.com>>"
 
-  echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_BUILD_NUMBER"; 
-  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_BUILD_NUMBER;
+  echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_TAG";
+  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_TAG;
 
   echo "Pushing image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag latest";
   docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
   docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME;
+else
+  echo "Not a Tag, so not pushing anything to Docker Hub!"
 fi
