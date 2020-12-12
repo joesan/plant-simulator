d8711ce8a70932145c379412630145c8b28edbf2
joesan <codeintheopen@gmail.com>
2020-12-12 22:54:23 +0100

    addad docker version

--- scripts/after_success_check.sh
+++ scripts/after_success_check.sh
@@ -4,15 +4,15 @@ if [ -n "$TRAVIS_TAG"  ]; then
   cd "${PLANT_SIMULATOR_DEPLOYMENT_REPO_NAME}"
   rm deployment-version.txt    # Remove the file
   touch deployment-version.txt # Add the file
-  echo plant-simulator.version=${TRAVIS_BUILD_NUMBER} >> deployment-version.txt
+  echo plant-simulator.version=${TRAVIS_TAG} >> deployment-version.txt
   # Install yq (https://github.com/mikefarah/yq) for inline yaml editing
   sudo wget https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq
   sudo chmod +x /usr/bin/yq
-  yq write --inplace helm-k8s/values.yaml 'app.version' ${TRAVIS_BUILD_NUMBER}
+  yq write --inplace helm-k8s/values.yaml 'app.version' ${TRAVIS_TAG}
   git config user.email "travis@travis.org"
   git config user.name ${USER} # this email and name can be set anything you like
   git add .
-  git commit --allow-empty -m "deployment plant-simulator tagged with version $TRAVIS_BUILD_NUMBER"
+  git commit --allow-empty -m "deployment plant-simulator tagged with version $TRAVIS_TAG"
   git push https://${GITHUB_API_KEY}@${GH_REPO} HEAD:master
 else
   echo "Skipping writing image tag release version to plant-simulator-deployment"
