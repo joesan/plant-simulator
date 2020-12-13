1ea770c21bd06727055426540a81dc66897a8f21
joesan <codeintheopen@gmail.com>
2020-12-13 01:06:46 +0100

    test

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #if [ $TRAVIS_BRANCH == "master" ]; then
 if [ -n "$TRAVIS_TAG"  ]; then
-  docker build -t . $DOCKER_APP_NAME -f docker/;
+  docker build . -t $DOCKER_APP_NAME -f docker/Dockerfile;
   docker images;
   docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_TAG;
 
