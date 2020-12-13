44f51e3d59213cb790f412820f92ed1cc363514f
joesan <codeintheopen@gmail.com>
2020-12-13 08:10:57 +0100

    added tags

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!bin/bash -e
 #if [ $TRAVIS_BRANCH == "master" ]; then
 if [ -n "$TRAVIS_TAG"  ]; then
   docker build . -t $DOCKER_APP_NAME -f docker/Dockerfile;
--- scripts/before_install_check.sh
+++ scripts/before_install_check.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!bin/bash -e
 if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(.md)|(.png)|(.pdf)|(.jpg)|(.jpeg)|(.html)|^(LICENSE)|^(docs)'
 then
   echo "Only doc files were updated, not running the CI."
