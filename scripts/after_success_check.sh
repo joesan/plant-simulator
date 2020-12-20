5f37bf4295c1f240b96678367752460c1924d5ba
joesan <codeintheopen@gmail.com>
2020-12-20 10:37:37 +0100

    issue#10

--- scripts/after_success_check.sh
+++ scripts/after_success_check.sh
@@ -3,6 +3,8 @@
 if [ -n "$TRAVIS_TAG"  ]; then
   git clone https://${GH_REPO}
   cd "${PLANT_SIMULATOR_DEPLOYMENT_REPO_NAME}"
+  # create a new branch with the TRAVIS_TAG as the name of the branch
+  git checkout -b feature-$TRAVIS_TAG
   rm deployment-version.txt    # Remove the file
   touch deployment-version.txt # Add the file
   echo plant-simulator.version=${TRAVIS_TAG} >> deployment-version.txt
@@ -14,7 +16,7 @@ if [ -n "$TRAVIS_TAG"  ]; then
   git config user.name ${USER} # this email and name can be set anything you like
   git add .
   git commit --allow-empty -m "deployment plant-simulator tagged with version $TRAVIS_TAG"
-  git push https://${GITHUB_API_KEY}@${GH_REPO} HEAD:master
+  git push https://${GITHUB_API_KEY}@${GH_REPO} feature-$TRAVIS_TAG
 else
   echo "Skipping writing image tag release version to plant-simulator-deployment"
 fi
