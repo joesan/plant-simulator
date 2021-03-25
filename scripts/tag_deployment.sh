659b3b6279393e841eaea8fe6e06400604e5a66c
joesan <codeintheopen@gmail.com>
2021-03-25 08:15:10 +0100

    'added changes'

--- scripts/tag_deployment.sh
+++ scripts/tag_deployment.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 if [ -n "$GITHUB_REF"  ]; then
-  git clone https://${GH_REPO}
-  cd "${PLANT_SIMULATOR_DEPLOYMENT_REPO_NAME}"
+  git clone https://$GH_REPO
+  cd "$PLANT_SIMULATOR_DEPLOYMENT_REPO_NAME"
   # create a new branch with the TRAVIS_TAG as the name of the branch
   git checkout -b feature-$GITHUB_REF
   rm deployment-version.txt    # Remove the file
   touch deployment-version.txt # Add the file
-  echo plant-simulator.version=${GITHUB_REF} >> deployment-version.txt
+  echo plant-simulator.version=$GITHUB_REF >> deployment-version.txt
   # Install yq (https://github.com/mikefarah/yq) for inline yaml editing
   sudo wget https://github.com/mikefarah/yq/releases/download/3.4.1/yq_linux_amd64 -O /usr/bin/yq
   sudo chmod +x /usr/bin/yq
-  yq write --inplace helm-k8s/values.yaml 'app.version' ${GITHUB_REF}
+  yq write --inplace helm-k8s/values.yaml 'app.version' $GITHUB_REF
   git config user.email "travis@travis.org"
-  git config user.name ${USER} # this email and name can be set anything you like
+  git config user.name $USER # this email and name can be set anything you like
   git add .
   git commit --allow-empty -m "deployment plant-simulator tagged with version $GITHUB_REF"
-  git push https://${GITHUB_API_KEY}@${GH_REPO} feature-$GITHUB_REF
+  git push https://$GITHUB_API_KEY@$GH_REPO feature-$GITHUB_REF
 else
   echo "Skipping writing image tag release version to plant-simulator-deployment"
 fi
