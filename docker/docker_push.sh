d30dfd38abceafb7d20ada875c03b13246cc9c51
joesan <codeintheopen@gmail.com>
2021-03-28 14:08:50 +0200

    'added Docker push changes'

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -4,23 +4,24 @@ echo "Pushing image to Docker Hub"
 echo "--------------------------------"
 echo "DOCKER_APP_NAME          = $DOCKER_APP_NAME"
 echo "DOCKER_REGISTRY_USERNAME = $DOCKER_REGISTRY_USERNAME"
+echo "DOCKER_REGISTRY_URL      = $DOCKER_REGISTRY_URL"
 echo "RELEASE TAG VERSION      = $RELEASE_VERSION"
 echo "--------------------------------"
 echo
 
-if [ -n "$GITHUB_REF"  ]; then
+if [ -n "$RELEASE_VERSION"  ]; then
   docker build . -t $DOCKER_APP_NAME -f docker/Dockerfile;
   docker images;
 
   echo "$DOCKER_REGISTRY_PASSWORD" | docker login -u "$DOCKER_REGISTRY_USERNAME" --password-stdin docker.io
-  echo "Successfully logged into Docker hub <<hub.docker.com>>"
+  echo "Successfully logged into Docker hub $DOCKER_REGISTRY_URL"
 
   # Tag & push image for tag $TRAVIS_TAG
-  echo "Tag image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_TAG";
-  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_TAG;
-  echo "Push image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_TAG";
-  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$TRAVIS_TAG;
-  echo "Successfully tagged and pushed image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $TRAVIS_TAG"
+  echo "Tag image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $RELEASE_VERSION";
+  docker tag $DOCKER_APP_NAME $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$RELEASE_VERSION;
+  echo "Push image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $RELEASE_VERSION";
+  docker push $DOCKER_REGISTRY_USERNAME/$DOCKER_APP_NAME:$RELEASE_VERSION;
+  echo "Successfully tagged and pushed image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag $RELEASE_VERSION"
 
   # Tag & push image for tag latest
   echo "Tag image $DOCKER_APP_NAME to repository $DOCKER_REGISTRY_URL with tag latest";
