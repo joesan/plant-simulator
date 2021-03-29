406ff19322b2825777f3f4a18c34d95bc11ac1b7
joesan <codeintheopen@gmail.com>
2021-03-29 20:02:41 +0200

    'added changes'

--- docker/docker_credentials_helper.sh
+++ docker/docker_credentials_helper.sh
@@ -7,14 +7,16 @@ echo "-----------------------------------------------------"
 echo "Setting up docker credential pass"
 echo "-----------------------------------------------------"
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt -y install docker-ce pass
-echo 'DOCKER_OPTS="--experimental"' | sudo tee /etc/default/docker
-sudo service docker restart
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt update
+#sudo apt -y install docker-ce pass
+sudo apt-get install pass
+#echo 'DOCKER_OPTS="--experimental"' | sudo tee /etc/default/docker
+#sudo service docker restart
 
-curl -fsSlL https://github.com/docker/docker-credential-helpers/releases/tag/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz | sudo tar xf - -C /usr/local/bin
-mkdir -p /home/travis/.docker
+#curl -fsSlL https://github.com/docker/docker-credential-helpers/releases/tag/v0.6.3/docker-credential-pass-v0.6.3-amd64.tar.gz | sudo tar xf - -C /usr/local/bin
+wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz && tar -xf docker-credential-pass-v0.6.0-amd64.tar.gz && chmod +x docker-credential-pass && sudo mv docker-credential-pass /usr/local/bin/
+mkdir -p "$HOME"/.docker
 echo '{ "credsStore": "pass" }' | tee "$HOME"/.docker/config.json
 gpg --batch --gen-key <<-EOF
 %echo generating a standard key
